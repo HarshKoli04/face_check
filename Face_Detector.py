dc820a384e0bbbf81bdc3c90de2e6722658cce96
harsh <kolih453@gmail.com>
2023-09-06 22:28:05 +0530

    first commit

---
+++ Face_Detector.py
@@ -0,0 +1,28 @@
+import cv2
+from random import randrange
+
+
+trained_face_data=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# img = cv2.imread("try.jpg")
+webcam=cv2.VideoCapture(0)
+
+while True:
+    successful_frame_read, frame = webcam.read()
+
+    grayscaled_img=cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    face_coordinates =trained_face_data.detectMultiScale(grayscaled_img)
+
+    for (x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,256),5)
+# randrange(256),randrange(256),randrange(256)
+    cv2.imshow("Harsh img",frame)
+
+    key=cv2.waitKey(1)
+
+    if key==81 or key==113:
+        break
+
+
+print("Code Completed")
